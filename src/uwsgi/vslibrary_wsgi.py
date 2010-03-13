6d4710a77dc071ac1b4404ecb7503bcc6f868d47
Viliam Segeda <viliam@vasudevaserver.org>
2010-03-13 13:23:19 +0100

    uwsgi reconfig

--- src/uwsgi/vslibrary_wsgi.py
+++ src/uwsgi/vslibrary_wsgi.py
@@ -1,5 +1,5 @@
 import os
 from paste.deploy import loadapp
 
-ini = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'production.ini')
+ini = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), 'production.ini')
 application = loadapp('config:%s' % ini)
