bcbf92415aab54ae452ee5eef5d0d1734b697da4
Viliam Segeda <viliam@vasudevaserver.org>
2010-03-18 23:12:37 +0100

    working out

--- src/scripts/memcheck.py
+++ src/scripts/memcheck.py
@@ -2,6 +2,9 @@ import psutil
 
 HUP = 1
 
+def mb(size):
+    return size/(1024*1024)
+
 def get_ps(name, username=''):
     
     ps = []
@@ -22,23 +25,23 @@ def check(rsslimit=900*1024*1024):
         rss += int(p.get_memory_info()[0])
         
     if rss > rsslimit:
-        print '%dMB' % (rss/(1024*1024))
+        print '%dMB' % mb(rss)
         # find the parent one
         for p in ps:
             pp = p.parent
             if pp and pp.name == p.name:
                 # kill the parent
                 pp.kill(HUP)
-                print 'process %s restarted' % pp.id
+                print 'process %s restarted' % pp.pid
                 return
-    print 'OK - %dMB' % (rss/(1024*1024))
+    print 'OK - %dMB' % mb(rss)
                  
 def info():
     ps = get_ps(name='uwsgi', username='vslib')
-    print "PID,\tPPID,\tCPU,\tMEM"
+    print "PID,\tPPID,\tCPU,\tRSS - \tVMS"
     for p in ps:
         rss, vms = p.get_memory_info()
-        print "%s\t%s\t%s%%\t%s - %s" % (p.pid, p.ppid, p.get_cpu_percent(), rss, vms) 
+        print "%s\t%s\t%s%%\t%s - \t%s" % (p.pid, p.ppid, p.get_cpu_percent(), mb(rss), mb(vms)) 
     
 if __name__=='__main__':
     info()
