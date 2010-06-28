15b14e9e03da9986ee59469202162cef45690c81
Viliam Segeda <viliam@vasudevaserver.org>
2010-06-28 21:15:22 +0200

    jsontest refactor

--- src/worker/jsontest.py
+++ src/worker/jsontest.py
@@ -1,7 +1,8 @@
+import sys
 from log import log
 from jsonrpc import Client
 
-host = "http://localhost:6543/"
+host = "http://localhost:7007/"
 
 def send(adr, **kw): 
 
@@ -13,7 +14,14 @@ def send(adr, **kw):
     
     
 if __name__=='__main__':
-    
-    data = dict(cmd='update', arg='0000')
-    #print send('put', **data)
-    print send('get')
+
+    data = {}
+    if len(sys.argv) > 2:
+        msg = sys.argv[2]
+        cmd, arg = msg.split(':') 
+        data = dict(cmd=cmd, arg=arg)
+    if len(sys.argv) > 1:
+        jcmd = sys.argv[1]
+        print send(jcmd, **data)
+    else:
+        print "Usage: %s cmd msg   /  cmd=(put/get) msg=cmd:arg" % sys.argv[0]
