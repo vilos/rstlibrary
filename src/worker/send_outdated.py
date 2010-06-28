e4212628f78004a8afea64a29f8cdd1bab3f08e7
Viliam Segeda <viliam@vasudevaserver.org>
2010-06-28 23:22:16 +0200

    send_outdated script

---
+++ src/worker/send_outdated.py
@@ -0,0 +1,31 @@
+import sys, os
+from svnup import SvnCommand
+from jsontest import send
+
+def getid(path):
+    base = os.path.basename(path)
+    name, ext = os.path.splitext(base)
+    if ext and ext == '.txt':
+        return name
+    return None
+    
+    
+if __name__=='__main__':
+    arg = ''
+    argc = len(sys.argv)
+    if (argc > 1):
+        
+        path = sys.argv[1]
+        
+        svn = SvnCommand(path)
+        files = svn.st()
+        outdated = [ f.path for f in files ]
+        
+        ids = [getid(p) for p in outdated]
+        
+        for id in ids:
+            send('put', 'update:%s' % id)
+            send('put', 'invalidate:%s' % id)
+            
+    else:
+        print "Usage: %s repo_path " % sys.argv[0]
