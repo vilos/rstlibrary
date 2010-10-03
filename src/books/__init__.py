77d31324c8b27659ffb005aa44ffd177ab44a9fd
Viliam Segeda <viliam@vasudevaserver.org>
2010-10-03 22:13:16 +0200

    added initialization methods for library and books modules

--- src/books/__init__.py
+++ src/books/__init__.py
@@ -1,5 +1,15 @@
-
+import library
 from source import register_source
 from store import register_store
-from models import Book
+from models import Book, get_book
 
+
+def configure(inifile=None):
+    config = library.configure(inifile)
+    src_path = config.get('src_path')
+    store_url = config.get('store_url')
+    cache_url = config.get('cache_url')
+    max_entries = config.get('max_entries')
+    register_source(src_path)
+    register_store(store_url, cache_url, max_entries=max_entries)
+    return config
--- src/library/__init__.py
+++ src/library/__init__.py
@@ -1,2 +1,13 @@
-# A package
+from paste.deploy.loadwsgi import appconfig
+import os
 
+DEFAULT_INIFILE = 'develop'
+
+def configure(inifile=None):
+    if not inifile:
+        inifile = DEFAULT_INIFILE
+    path = '%s.ini' % inifile
+    config_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..', path))
+    base, name = os.path.split(config_path)
+    config = appconfig('config:%s' % name, name='vslibrary', relative_to=base)
+    return config
